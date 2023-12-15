0c6703c01368fc9c61907a5c7e7c86efbb99d503
WindrunnerMax <651525974@qq.com>
2023-12-15 20:05:33 +0800

    feat: mobile vconsole

--- packages/webrtc/client/index.tsx
+++ packages/webrtc/client/index.tsx
@@ -2,6 +2,12 @@ import ReactDOM from "react-dom";
 import { App } from "./components/app";
 import "@arco-design/web-react/es/style/index.less";
 import { drawingBackdrop } from "./components/canvas";
+import { isMobile } from "./utils/is";
+import VConsole from "vconsole";
+
+if (process.env.NODE_ENV === "development" && isMobile()) {
+  new VConsole();
+}
 
 window.addEventListener("DOMContentLoaded", drawingBackdrop);
 ReactDOM.render(<App></App>, document.getElementById("root"));
--- packages/webrtc/types/global.d.ts
+++ packages/webrtc/types/global.d.ts
@@ -2,3 +2,9 @@ declare module "*.scss" {
   const content: Record<string, string>;
   export default content;
 }
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: "development" | "production";
+  }
+}
